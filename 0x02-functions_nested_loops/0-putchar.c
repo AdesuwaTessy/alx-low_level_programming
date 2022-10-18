8438242172662ca348673b891cc9050e7eb14608
AdesuwaTessy <usenbortessy@gmail.com>
2022-10-18 15:47:09 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * main - Print _putchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char text[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
