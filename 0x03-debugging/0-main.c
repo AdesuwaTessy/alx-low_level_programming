50e0abf460bb20059d2e72a05e583dbdd5b09fd3
AdesuwaTessy <usenbortessy@gmail.com>
2022-10-19 13:09:19 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - a function that prints positive or negative integers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98 % 2;
+
+	positive_negative(i);
+
+	return (0);
+}
